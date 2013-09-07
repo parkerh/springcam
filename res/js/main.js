b4bdb93bc54a18b560704f130e9c9795541a08b2
Parker Henry <parkerhenry0@gmail.com>
2013-09-07 14:17:51 -0400

    I have a moving ball.

--- res/js/main.js
+++ res/js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
   var paper = new Raphael(document.getElementById("canvas_container"),
     500,500);
+  var ball = paper.circle(50, 50, 40);
+  ball.attr({"gradient": "90-#526c7a-#64a0c1"});
+
+  var move_ball = function () {
+    ball.attr({
+      "transform" : "t 0 0",
+      "cx" : ball.attr("cx") + 10}).animate(
+      {"transform" : ["t", 10, 0]}, 1000, move_ball);
+  };
+
+  move_ball();
 });
+
