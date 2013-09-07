8ee76ba0ba319de0b7a6c2c6538c6fad696e5e19
Parker Henry <parkerhenry0@gmail.com>
2013-09-07 14:36:40 -0400

    Got ball to follow rudimentary overdamped equation.

--- res/js/main.js
+++ res/js/main.js
@@ -4,13 +4,21 @@ $(document).ready(function() {
   var ball = paper.circle(50, 50, 40);
   ball.attr({"gradient": "90-#526c7a-#64a0c1"});
 
+  var time = 0;
+
+  var equation = overdamped_equation(50,-3,-2,-7);
+
   var move_ball = function () {
+    var posn1 = equation(time) * 500;
+    time += 1;
+    var posn2 = equation(time) * 500;
     ball.attr({
       "transform" : "t 0 0",
-      "cx" : ball.attr("cx") + 10}).animate(
-      {"transform" : ["t", 10, 0]}, 1000, move_ball);
+      "cy" : posn1
+    }).animate(
+      {"transform" : ["t", 0, posn2 - posn1]}, 1000, move_ball);
   };
 
-  move_ball();
+  move_ball(); 
 });
 
