125a015f3da99ae07a12432cdeda8c559e2607b1
Parker Henry <parkerhenry0@gmail.com>
2013-09-07 05:15:32 -0400

    Fixed underdamping to account for real component of the roots.

--- res/js/equations.js
+++ res/js/equations.js
@@ -14,14 +14,16 @@ var overdamped_equation = function(c_1,c_2,r_1,r_2) {
   };
 };
 
-var underdamped_equation = function(c_1,c_2,r_1,r_2) {
+var underdamped_equation = function(c_1,c_2,r_1,r_2,r_r) {
   return function(time) {
     var a = r_1 * time;
     var b = r_2 * time;
+    var e = Math.pow(Math.e, r_r * time);
 
     /* Math.cos and Math.sin take arguments in radians. */
-    var posn = c_1 * Math.cos(a) +
-      c_2 * Math.sin(b);
+    var posn = e * 
+      (c_1 * Math.cos(a) +
+      c_2 * Math.sin(b));
     return posn;
   };
 };
