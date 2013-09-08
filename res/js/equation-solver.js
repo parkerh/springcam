63d5ba5387e5edabe3d06819bd3e47d2fece26db
Parker Henry <parkerhenry0@gmail.com>
2013-09-07 23:18:35 -0400

    Changed some values in equation-solver

--- res/js/equation-solver.js
+++ res/js/equation-solver.js
@@ -4,8 +4,8 @@ var equation_solver = function(c,k) {
   var r_1 = roots[0];
   var r_2 = roots[1];
 
-  var c_1 = 10 * r_2 / (r_2 - r_1);
-  var c_2 = 10 * r_1 / (r_1 - r_2);
+  var c_1 = (100 * r_2 + 100)/ (r_2 - r_1);
+  var c_2 = (100 * r_1 + 100)/ (r_1 - r_2);
 
   return [c_1,c_2,r_1,r_2];
 }
